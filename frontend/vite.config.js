16b79f31f576cff25c5616b515b20fa2bbc21ab2
Neira053 <nehapaswan2806@gmail.com>
2026-05-11 18:28:38 +0530

    updated app.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     port: 3000,
+    
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://cloudsentinel-imm0.onrender.com',
         changeOrigin: true,
+        secure: true,
       }
     }
   }
